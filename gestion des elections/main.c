9ef5085f71a4d79b171fd119eb89c50685fbf06b
syrineahmed <115407216+syrineahmed@users.noreply.github.com>
2022-11-07 10:39:19 +0100

    Update main.c

--- gestion des elections/main.c
+++ gestion des elections/main.c
@@ -1,16 +1,15 @@
-include <stdio.h>
-#include <string.h>
+#include <stdio.h>
 #include"election.h"
 
 int main()
 {
-    election identifiant , Date , Types d'élèctions,Ville,Gouvernorat,Population,Conseillères;
-    int x=ajouter("election.txt", identifiant, Date , Types      				     d'élèctions,Ville,Gouvernorat,Population,Conseillères);
+    election E= (12865013,12/15/2022,"Tunis","Ariana",1500,25,"Ariana");
+    int x=ajouter("election.txt", E);
     
     if(x==1)
         printf("\najout de point avec succés");
     else printf("\nechec ajout");
-        */
+        /*
     x=modifier("election.txt",1, identifiant , Date , Types 					   	d'élèctions,Ville,Gouvernorat,Population,Conseillères);
 
     if(x==1)
@@ -22,6 +21,6 @@ int main()
     else printf("\nechec Suppression");
     E =chercher("election.txt",3 );
     if(E.identifiant==-1)
-        printf("introuvable");
+        printf("introuvable");*/
     return 0;
 }
