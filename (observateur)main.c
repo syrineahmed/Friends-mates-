8c86303cd989b782bf5966d893f4426bf015abb0
hamouda123456 <115362965+hamouda123456@users.noreply.github.com>
2022-11-09 20:26:26 +0100

    Update (observateur)main.c

--- (observateur)main.c
+++ (observateur)main.c
@@ -4,23 +4,23 @@
 
 int main()
 {
-    obsevateur O1= {1111111,15,5,1970,"MOHSEN","GHORBEL","NON ETRANGER","TECHNICIEN","HOMME"},O2= {1111112,15,5,1970,"SOUMAYA","OUNI","NON ETRANGER","TECHNICIENNE","FEMME"},O3;
-    int x=ajouter( O1,"observateur.txt");
+    observateur O1= {1111111,15,5,1970,"MOHSEN","GHORBEL","NON ETRANGER","TECHNICIEN","HOMME"}; observateur O2= {1111112,15,5,1970,"SOUMAYA","OUNI","NON ETRANGER","TECHNICIENNE","FEMME"},O3;
+    int x=ajouter( O1,"Observateur.txt");
     
     if(x==1)
         printf("\najout de OBSERVATEUR avec succés");
     else printf("\nechec ajout");
         
-    x=modifier(1,O2,"observateur.txt" );
+    x=modifier(1,O2,"Observateur.txt" );
 
     if(x==1)
         printf("\nModification de OBSERVATEUR avec succés");
     else printf("\nechec Modification");
-    x=supprimer(1 ,"observateur.txt");
+    x=supprimer(1 ,"Observateur.txt");
     if(x==1)
         printf("\nSuppression de OBSERVATEUR avec succés");
     else printf("\nechec Suppression");
-    O3=chercher(3,"observateur.txt" );
+    O3=chercher(3,"Observateur.txt" );
     if(O3.Id==-1)
         printf("introuvable");
     return 0;
